dbd6cb0465aa2d5e9946a8ba6c3bcd603993013a
UzBro <97809579+UzBro@users.noreply.github.com>
2022-01-28 19:32:09 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,59 +1,86 @@
 const TelegramApi = require('node-telegram-bot-api')
 const {gameOptions, againOptions} = require('./options')
-
+const {MongoClient} = require('mongodb');
+const client = new MongoClient(`mongodb+srv://cloud:${process.env.PASS}@cloud.o8s0t.mongodb.net/cloud?retryWrites=true&w=majority`);
 
 const bot = new TelegramApi(process.env.BOT_TOKEN, {polling: true})
 
 const chats = {}
-
-
+const Users = client.db().collection('tgbot')
 
 const startGame = async (chatId) => {
+	if (!await Users.findOne({id: `${chatId}`})) {
+			await Users.insertOne({id: `${chatId}`, name: `${msg.from.first_name}`, right: 0, wrong: 0})
+		}
     await bot.sendMessage(chatId, `Men 0 dan 9 gacha raqam o'ylayman, sen topishga urinib ko'r)`);
     const randomNumber = Math.floor(Math.random() * 10)
     chats[chatId] = randomNumber;
     await bot.sendMessage(chatId, 'Qanaqa son o`yladm?', gameOptions);
 }
 
-const start = () => {
-bot.setMyCommands([
-  {command: '/start', description: 'Salom'},
-  {command: '/info', description: 'Ma`lumot'},
-  {command: '/game', description: 'Raqamni top'},
-  ])
+const start = async () => {
 
-bot.on('message', async msg => {
-  const text = msg.text;
-  const chatId = msg.chat.id;
-  
-  if (text === '/start'){
-      await bot.sendMessage(chatId, 'https://tlgrm.ru/_/stickers/ea5/382/ea53826d-c192-376a-b766-e5abc535f1c9/7.webp');
-      return bot.sendMessage(chatId, `Salom men Telegram bot! Bu Axrorning birinchi dasturi!!!`);
-  }
-  if (text === '/info'){
-      return bot.sendMessage(chatId, `Sening isming ${msg.from.first_name} ${msg.from.last_name}`);
-  }
-  if (text === '/game'){
-    return startGame(chatId)
-  }
-  
-  return bot.sendMessage(chatId, 'Nma deb yozganingni tushuna olmadm, yana bir bor urinib kor!)');
-})
+    try {
+        await client.connect()
+	
+    } catch (e) {
+        console.log('Baza daniyga ulanib bolmadi', e)
+    }
 
-bot.on('callback_query', async msg => {
-  const data = msg.data;
-  const chatId = msg.message.chat.id;
-  if (data === '/again') {
-    return startGame(chatId)
-  }
-  if (data == chats[chatId]) {
-    return bot.sendMessage(chatId, `Kallangga qoilm)) topib qo'yding!! ${chats[chatId]} to'g'ri topding`, againOptions)
-  } else {
-    return bot.sendMessage(chatId, `Topa olmading, men ${chats[chatId]} sonini o'ylagandim`, againOptions)
-  }
-  
-})
+    bot.setMyCommands([
+        {command: '/start', description: 'Salom'},
+        {command: '/info', description: 'Ma`lumot'},
+        {command: '/game', description: 'Raqamni top'},
+    ])
 
+    bot.on('message', async msg => {
+        const text = msg.text;
+        const chatId = msg.chat.id;
+
+        try {
+            if (text === '/start') {
+		if (!await Users.findOne({id: `${chatId}`})) {
+			await Users.insertOne({id: `${chatId}`, name: `${msg.from.first_name}`, right: 0, wrong: 0})
+		}
+                await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/ea5/382/ea53826d-c192-376a-b766-e5abc535f1c9/7.webp')
+                return bot.sendMessage(chatId, `Salom men Telegram bot! Bu Axrorning birinchi dasturi!!!`);
+            }
+            if (text === '/info') {
+		if (!await Users.findOne({id: `${chatId}`})) {
+			await Users.insertOne({id: `${chatId}`, name: `${msg.from.first_name}`, right: 0, wrong: 0})
+		}
+                const user = await Users.findOne({id: `${chatId}`})
+                return bot.sendMessage(chatId, `Sening isming ${msg.from.first_name} ${msg.from.last_name}, togri topilgan javoblar ${user.right}, notogrisi ${user.wrong}`);
+            }
+            if (text === '/game') {
+                return startGame(chatId);
+            }
+            return bot.sendMessage(chatId, 'Nma deb yozganingni tushuna olmadm, yana bir bor urinib kor!)');
+        } catch (e) {
+		console.log('Baza daniyga ulanib bolmadi', e)
+            return bot.sendMessage(chatId, 'Qandaydir nosozlik programmistga habar bering!');
+        }
+
+    })
+
+    bot.on('callback_query', async msg => {
+        const data = msg.data;
+        const chatId = msg.message.chat.id;
+        if (data === '/again') {
+            return startGame(chatId)
+        }
+        let user = await Users.findOne({id: `${chatId}`})
+        if (data == chats[chatId]) {
+		++user.right
+            await Users.updateOne({id: `${chatId}`},{$set: {right:`${user.right}`}});
+            await bot.sendMessage(chatId, `Kallangga qoilm)) topib qo'yding!! ${chats[chatId]} to'g'ri topding`, againOptions);
+        } else {
+		++user.wrong
+            await Users.updateOne({id: `${chatId}`},{$set: {wrong:`${user.wrong}`}});
+            await bot.sendMessage(chatId, `Topa olmading, men ${chats[chatId]} sonini o'ylagandim`, againOptions);
+        }
+      
+    })
 }
 
 start()
